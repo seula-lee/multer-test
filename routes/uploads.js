84ccbe07e57e80398f5ab505b8579b30a25d1803
seula-lee <salee8909@gmail.com>
2016-11-08 16:18:20 +0000

    connected with google api

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var multer  = require('multer');
 var fs      = require('fs');
+var Vision = require('@google-cloud/vision');
+// Instantiate a vision client
+var vision = Vision({
+  projectId: 'logging-life',
+  keyFilename: './api/logging-life.json',
+});
+
 var router = express.Router();
 var storage =   multer.diskStorage({
   destination: function (req, file, callback) {
@@ -10,13 +17,48 @@ var storage =   multer.diskStorage({
     callback(null, file.fieldname + '-' + Date.now()+'.'+file.mimetype.split('/')[1]);
   }
 });
+
 var upload = multer({storage: storage});
 
 router.post('/', upload.any() , function(req, res, next){
   console.log(req.files);  // debug
+  var inputFile = req.files[0].destination+req.files[0].filename;
+
+  // call google vision api
+  detectLabels(inputFile, function (err, labels) {
+    if (err) {
+      return callback(err);
+    }
+    console.log('Found label: ' + labels[0].desc + ' for ' + inputFile);
+    console.log(labels);  // debug
+  });
+
   res.render('uploads', { src:"./uploads/"+req.files[0].filename });
-	res.status(204).end();
+  res.status(204).end();
 });
+
+// GOOGLE VISION API
+function detectLabels (inputFile, callback) {
+  // Make a call to the Vision API to detect the labels
+  vision.detectLabels(inputFile, { verbose: true }, function (err, labels) {
+    if (err) {
+      return callback(err);
+    }
+    // console.log('result:', JSON.stringify(labels, null, 2)); // debug
+    callback(null, labels);
+  });
+}
+
+// [START run_application]
+if (module === require.main) {
+  if (process.argv.length < 3) {
+    console.log('Usage: node labelDetection <inputFile>');
+    process.exit(1);
+  }
+  var inputFile = process.argv[2];
+  main(inputFile, console.log);
+}
+
 
 module.exports = router;
 
